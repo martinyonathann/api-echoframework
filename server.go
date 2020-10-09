606e4f21bf7a077b871c820cd11c08c8b08cf569
NB_Martin <martin.y@inception.asia>
2020-10-09 20:24:42 +0700

    clear crud echo from https://echo.labstack.com/cookbook/crud

--- server.go
+++ server.go
@@ -1,40 +1,75 @@
 package main
+
 import (
-	//Echo framework library
 	"net/http"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
+
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+//----------
+// Handlers
+//----------
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
 
 func main() {
 	e := echo.New()
 
 	// Middleware
-	// e.Use(middleware.Logger())
+	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: `"${time_rfc3339}" with Response {"id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
-		`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}","latency":${latency},` +
-		`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
-		`"bytes_out":${bytes_out}}` + "\n",
-	  }))
+	// Routes
+	e.POST("/users", createUser)
+	e.GET("/users/:id", getUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
 
-	//CORS
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-	}))
-
-	//Route => Handler
-	e.GET("/", func (c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
-
-	//Server
+	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
-// // Route => handler  
-// e.GET("/", func(c echo.Context) error {       
-// 	return c.String(http.StatusOK, "Hello, World!\n")  
-// })
